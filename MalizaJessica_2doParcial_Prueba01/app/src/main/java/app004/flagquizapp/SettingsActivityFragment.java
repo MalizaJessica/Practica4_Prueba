3677b648a1224020f07792afe1b1ef50fb963307
MalizaJessica <jessicamaliza19@gmail.com>
2022-01-26 17:29:41 -0500

    Commit 17:30

---
+++ MalizaJessica_2doParcial_Prueba01/app/src/main/java/app004/flagquizapp/SettingsActivityFragment.java
@@ -0,0 +1,17 @@
+package app004.flagquizapp;
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+import android.support.annotation.Nullable;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class SettingsActivityFragment extends PreferenceFragment {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(app004.flagquizapp.R.xml.preferences);
+    }
+}
